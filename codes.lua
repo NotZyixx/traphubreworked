8ee4c653ed3fed532d701079a9bccf32fbd29b85
NotZyixx <123068029+NotZyixx@users.noreply.github.com>
2024-04-28 21:03:26 +0300

    Add files via upload

---
+++ codes.lua
@@ -0,0 +1,53 @@
+-- codes.lua
+
+getgenv().codes = {
+    "QUESTFIX",
+    "HOLLOW",
+    "subtomaokuma",
+    "SubToKelvingts",
+    "SubToBlamspot",
+    "TOADBOIGAMING",
+    "KingLuffy",
+    "noclypso",
+    "subtosnowrbx",
+    "Cxrsed",
+    "FictioNTheFirst",
+}
+
+getgenv().codesbf = {
+    "Sub2Fer999",
+    "Enyu_is_Pro",
+    "Magicbus",
+    "JCWK",
+    "Starcodeheo",
+    "Bluxxy",
+    "SUB2GAMERROBOT_EXP1",
+    "Sub2NoobMaster123",
+    "Sub2Daigrock",
+    "Axiore",
+    "TantaiGaming",
+    "StrawHatMaine",
+    "Sub2OfficialNoobie",
+    "TheGreatAce"
+}
+
+getgenv().codesperoxide = {
+				'RELEASE!!!!',
+				'40kLikesRobloxAPICantHandle60k',
+				'50kLikesWowThatWasFast',
+				'10MVisits!',
+				'30kLikesThanksForSupportingUs',
+				'60kLikesHiGuys',
+				'AmogusGrimaceShake',
+				'80kLikesSorryForACStrikes',
+				'100kLikesYouAllAreEatingGood',
+				'120kLikesHolyMoly',
+				'130kLikesInfiniteCodes',
+				'40MVisits!!!',
+				'30MVisits!!!',
+				'140kLikesNewContentSoon',
+				'HairRatsMatter155!',
+				'ThxForFollowingTyDev10!',
+				'CodeGoblins',
+				'50MVisits!?!?!Wow!!',
+			}
